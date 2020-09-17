f22a00473939415988f49ddb3d5023a5f55eca80
iantheninja <munene.ian@live.com>
2020-09-17 18:18:33 +0300

    dropdown markup added

--- scripts.js
+++ scripts.js
@@ -5,8 +5,20 @@ const showMenu = (toggleId, navId) => {
   if (toggle && nav) {
     toggle.addEventListener('click', () => {
       nav.classList.toggle('show')
+      toggle.classList.toggle('bx-x')
     })
   }
 }
 
 showMenu('header_toggle', 'nav_menu')
+
+// active and remove menu: when you click on link in side menu bar
+
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+  navLink.forEach(n => n.classList.remove('active'))
+  this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
